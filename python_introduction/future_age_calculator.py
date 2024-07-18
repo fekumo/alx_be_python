d006aa7e98efa0d35ad3fcd8111a6a6fc5ef194d
fekumo <135237133+fekumo@users.noreply.github.com>
2024-07-18 14:19:05 +0100

    Delete python_introduction/future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-user_current_age = int(input("Enter your current age: "))
-
-user_future_age = user_current_age + 27
-
-print("In 2050, you will be", user_future_age, "years old.")
