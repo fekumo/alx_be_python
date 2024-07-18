4c0919dcbed9501e7281d8ebd45605b95d7b8c69
fekumo <135237133+fekumo@users.noreply.github.com>
2024-07-18 14:19:31 +0100

    addon

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+user_current_age = int(input("How old are you?: "))
+
+user_future_age = user_current_age + 27
+
+print("In 2050, you will be", user_future_age, "years old.")
