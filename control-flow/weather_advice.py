a599c5fa23b6139b5a6e873b74aee260aa27fe3c
fekumo <135237133+fekumo@users.noreply.github.com>
2024-07-23 15:05:11 +0100

    auto

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-weather_condition = input("What's the weather like today? (sunny/rainy/cold): ")
+weather_condition = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
 sunny = "sunny"
 rainy = "rainy"
@@ -9,9 +9,9 @@ cold = "cold"
 
 if weather_condition == sunny:
    print("Wear a t-shirt and sunglasses.")
-elif rainy:
+elif weather_condition == rainy:
    print("Don't forget your umbrella and a raincoat.")
-elif cold:
+elif weather_condition == cold:
    print("Make sure to wear a warm coat and a scarf.")
 else:
    print("Sorry, I don't have recommendations for this weather")
