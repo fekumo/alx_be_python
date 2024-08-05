1e2182d2582103a4c4404dbe64249ef5a9b4e312
fekumo <simondemisinclair@gmail.com>
2024-08-05 04:05:49 -0700

    Added a file

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def display_menu():
-    print("\nShopping List Manager")
+    print(f"Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
